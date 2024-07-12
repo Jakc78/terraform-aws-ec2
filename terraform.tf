847b8dd87adb2fa3794197d3d74eb29db2c1e585
Jakc78 <65726566+Jakc78@users.noreply.github.com>
2024-07-12 10:44:50 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = "~> 1.9.2"
 
-  cloud {
-    organization = "EI"
-
-    workspaces {
-      name = "Training"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
